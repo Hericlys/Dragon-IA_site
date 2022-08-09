3d6d6c6e52f1de8185d05c6cc5dcf4a2e8fb15d3
hericlys <Hericlysdesa@gmail.com>
2022-08-09 10:37:00 -0300

    mensagens add

--- paginas/views.py
+++ paginas/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.contrib import messages
 
 
 def cadastro(request):
+    messages.add_message(request, messages.INFO, 'chave')
     return render(request, 'paginas/cadastro.html')
 
 
