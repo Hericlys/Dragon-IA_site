cf03389b081cc5b904b60353b47f2d77350aae3d
hericlys <Hericlysdesa@gmail.com>
2022-10-24 12:46:10 -0300

    static root add

--- config/settings.py
+++ config/settings.py
@@ -121,6 +121,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'templates/static',
 ]
+STATIC_ROOT = BASE_DIR / 'static'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
